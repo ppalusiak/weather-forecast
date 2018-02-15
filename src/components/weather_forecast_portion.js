1a8b0d6c08f5256d2bac35f6fff6c7a32c64e4b9
Pawel Palusiak <Pawel.Palusiak@tipico.com>
2018-02-15 23:02:46 +0100

    clean html

--- src/components/weather_forecast_portion.js
+++ src/components/weather_forecast_portion.js
@@ -4,12 +4,12 @@ const WeatherForecastPortion = (props) => {
 
 
     return (
-        <li className="forecast-portion">
-            <p className="date">{props.data}</p>
-            <p className="min-temp">Min temperature: {props.minTemp}</p>
-            <p className="max-temp">Max temperature: {props.maxTemp}</p>
-            <p className="weather-indication">Weather ind: {props.indication}</p>
-        </li>
+        <section className="forecast-portion">
+            <h2 className="date">{props.data}</h2>
+            <div className="min-temp">Min temperature: {props.minTemp}</div>
+            <div className="max-temp">Max temperature: {props.maxTemp}</div>
+            <div className="weather-indication">Weather ind: {props.indication}</div>
+        </section>
     );
 };
 
