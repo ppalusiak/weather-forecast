3dec2ef923ff7c1f2a2066047175ffcd8f689d85
Pawel Palusiak <Pawel.Palusiak@tipico.com>
2018-02-15 22:59:46 +0100

    clean code, fix forecast date

--- src/components/weather_forecast.js
+++ src/components/weather_forecast.js
@@ -3,10 +3,9 @@ import WeatherForecastPortion from './weather_forecast_portion';
 
 const WeatherForecast = (props) => {
     const weatherForecastPortion = props.weatherForecast.map((weatherUnit) => {
-    console.log(weatherUnit.weather[0].main);
         return (
             <WeatherForecastPortion
-                data={weatherUnit.dt}
+                data={weatherUnit.dt_txt}
                 minTemp={weatherUnit.main.temp_min}
                 maxTemp={weatherUnit.main.temp_max}
                 indication={weatherUnit.weather[0].main}
